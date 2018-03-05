35778dd61d65f04c102cdb897fe1964936f2179f
phsestep <37028111+phsestep@users.noreply.github.com>
2018-03-05 09:55:34 -0500

    Update Bubble_Insertion_Selection.java

--- Bubble_Insertion_Selection.java
+++ Bubble_Insertion_Selection.java
@@ -33,6 +33,20 @@ public class Bubble_Insertion_Selection {
 		}
 	}
 	public static void bubbleSort(int[] list){
+		for(int i = 0; i < list.length; i++){
+			boolean inorder = true;
+			for(int j = 0; j < list.length - 1 - i; j++){
+				if(list[j] > list[j+1]){
+					int temp = list[j];
+					list[j] = list[j+1];
+					list[j+1] = temp;
+					inorder = false;
+				}
+			}
+			if(inorder)
+				return;
+		}
+	}
 		
 			
 	public static void main(String[] args) {
